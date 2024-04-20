63922be45aec299b6564da670b2e3e5bf35f14f8
maarfinn <asfahan.arfinn@gmail.com>
2024-04-21 00:13:43 +0530

    changes

--- src/Screens/SplashScreen.js
+++ src/Screens/SplashScreen.js
@@ -1,6 +1,5 @@
 import {View, Image, Dimensions, StyleSheet, Platform} from 'react-native';
 import React from 'react';
-import LinearGradient from 'react-native-linear-gradient';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -15,18 +14,10 @@ const SplashScreen = () => {
             uri: 'https://nkhhammers.com/wp-content/uploads/2022/05/50a623d1-003b-448d-b85c-ac03d5357a7c.jpg',
           }}
         />
-        <LinearGradient
-          colors={[
-            'rgba(255,255,255,0)',
-            'rgba(0,0,0,0.4)',
-            'rgba(0,0,0,0.5)',
-            'rgba(0,0,0,0.7)',
-            'rgba(0,0,0,0.8)',
-            'rgba(0,0,0,1)',
-            'rgba(0,0,0,1)',
-            'rgba(0,0,0,1)',
-          ]}
-          style={styles.gradient}></LinearGradient>
+        <Image
+          style={styles.gradient}
+          source={require('../Assets/shade1.png')}
+        />
       </View>
       <Image
         style={styles.imageContainerChild}
